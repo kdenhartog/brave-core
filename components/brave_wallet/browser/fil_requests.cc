0eefb1f6a40815ef54e8137b798b4adf741fb810
Sergey P <spylogsster@gmail.com>
2022-04-03 12:53:58 +0300

    Added Filecoin.StateSearchMsgLimited

--- components/brave_wallet/browser/fil_requests.cc
+++ components/brave_wallet/browser/fil_requests.cc
@@ -7,6 +7,7 @@
 #include "absl/types/optional.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
+#include "base/logging.h"
 #include "brave/components/brave_wallet/browser/json_rpc_requests_helper.h"
 #include "brave/components/json/rs/src/lib.rs.h"
 
@@ -98,6 +99,18 @@ std::string getChainHead() {
   return GetJsonRpcNoParams("Filecoin.ChainHead");
 }
 
+std::string getStateSearchMsgLimited(const std::string& cid, uint64_t period) {
+  base::Value cid_value(base::Value::Type::DICTIONARY);
+  cid_value.SetStringKey("/", cid);
+  auto result =
+      GetJsonRpc2Params("Filecoin.StateSearchMsgLimited", std::move(cid_value),
+                        base::Value(std::to_string(period)));
+  LOG(ERROR) << "result:" << result << "period" << std::to_string(period);
+  result = std::string(
+      json::convert_string_value_to_uint64("/params/1", result.c_str())
+          .c_str());
+  return result;
+}
 }  // namespace fil
 
 }  // namespace brave_wallet
